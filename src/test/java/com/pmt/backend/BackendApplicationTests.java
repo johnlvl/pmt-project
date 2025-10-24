2d934ac21fa69b134c5aa3b1decd549a1fb3e63a
John Leveil <leveiljohn@gmail.com>
2025-10-24 11:58:11 +0200

    test: exclude JDBC/JPA autoconfig in contextLoads to avoid datasource requirement on CI

--- src/test/java/com/pmt/backend/BackendApplicationTests.java
+++ src/test/java/com/pmt/backend/BackendApplicationTests.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+	"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
+})
 @ActiveProfiles("test")
 class BackendApplicationTests {
 
